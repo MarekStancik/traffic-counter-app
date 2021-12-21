9bf25a72676466f3fb8f1f0064e99cadf873f379
Marek Stančík <marekst11@gmail.com>
2021-12-21 11:30:30 +0100

    Refactor use observable hook

--- src/hooks/use-observable.hook.ts
+++ src/hooks/use-observable.hook.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import { Observable } from "rxjs";
 
-const useObservable = <T extends any>(source: Observable<T>): T | undefined => {
+const useObservable = <T extends any>(source: Observable<T>, ...deps: any): T | undefined => {
     const [data, setData] = useState<T | undefined>(undefined);
 
     useEffect(() => {
-        const subscription = source.subscribe({ next: data => setData(data as T) });
+        const subscription = source.subscribe({next: data => setData((Array.isArray(data) ? [...data] : typeof data === "object" ? Object.assign({}, data) : data) as T)});
         return () => {
             if (subscription) {
                 subscription.unsubscribe();
             }
         };
-    });
+    }, deps); // eslint-disable-line react-hooks/exhaustive-deps
 
     return data as T;
 };
